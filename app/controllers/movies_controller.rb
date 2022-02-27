08a544ba5aaf979da3f162fc0df5110ad39a6ea3
EC2 Default User <ec2-user@ip-172-31-4-17.ec2.internal>
2022-02-27 05:38:41 +0000

    commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,69 +1,70 @@
 class MoviesController < ApplicationController
 
-    def show
-      id = params[:id] # retrieve movie ID from URI route
-      @movie = Movie.find(id) # look up movie by unique ID
-      # will render app/views/movies/show.<extension> by default
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index #index.html
+    #@movies = Movie.all
+    ######added
+    @all_ratings = Movie.all_ratings #
+    @ratings_to_show = params[:ratings] || {} 
+    ratings_list = @ratings_to_show
+    session[:ratings]= @ratings_to_show #part3
+    if @ratings_to_show == {}
+      ratings_list = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
     end
-  
-    def index
-      #@movies = Movie.all
-      ######added
-      @all_ratings = Movie.all_ratings #
-      @ratings_to_show = params[:ratings] || {} 
-      ratings_list = @ratings_to_show
-      session[:ratings]= @ratings_to_show #part3
-      if @ratings_to_show == {}
-        ratings_list = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
-      end
     
-      #update movies filtered by ratings
+    #update movies filtered by ratings
     
-      @movies = Movie.with_ratings(ratings_list.keys)
-      ######
-      @clicked_header = params[:clicked_header] || "" #session[:clicked_header] || ""
-      session[:clicked_header] = @clicked_header #part3
-      #sort movies in order
-      if @clicked_header == "title_header"
-        @movies = @movies.order(:title)
-      end
-      if @clicked_header == "release_date_header"
-        @movies = @movies.order(:release_date)
-      end
+    @movies = Movie.with_ratings(ratings_list.keys)
+    ######
+    @clicked_header = params[:clicked_header] || "" #session[:clicked_header] || ""
+    session[:clicked_header] = @clicked_header #part3
+    #sort movies in order
+    if @clicked_header == "title_header"
+      @movies = @movies.order(:title)
     end
-  
-    def new
-      # default: render 'new' template
-    end
-  
-    def create
-      @movie = Movie.create!(movie_params)
-      flash[:notice] = "#{@movie.title} was successfully created."
-      redirect_to movies_path
-    end
-  
-    def edit
-      @movie = Movie.find params[:id]
-    end
-  
-    def update
-      @movie = Movie.find params[:id]
-      @movie.update_attributes!(movie_params)
-      flash[:notice] = "#{@movie.title} was successfully updated."
-      redirect_to movie_path(@movie)
-    end
-  
-    def destroy
-      @movie = Movie.find(params[:id])
-      @movie.destroy
-      flash[:notice] = "Movie '#{@movie.title}' deleted."
-      redirect_to movies_path
-    end
-  
-    private
-    # Making "internal" methods private is not required, but is a common practice.
-    # This helps make clear which methods respond to requests, and which ones do not.
-    def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    if @clicked_header == "release_date_header"
+      @movies = @movies.order(:release_date)
     end
   end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
