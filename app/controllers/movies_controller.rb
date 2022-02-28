8b4b242586508a7307ccddae7f3cc7c5583ab9ab
EC2 Default User <ec2-user@ip-172-31-4-17.ec2.internal>
2022-02-28 03:36:01 +0000

    commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,32 +1,36 @@
 class MoviesController < ApplicationController
 
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-  
-  def index
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      ordering,@title_header = {:title => :asc}, 'hilite'
-    when 'release_date'
-      ordering,@date_header = {:release_date => :asc}, 'hilite'
-    end
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-    
-    if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+
+  def index #index.html
+    #@movies = Movie.all
+    ######added
+    @all_ratings = Movie.all_ratings #
+    @ratings_to_show = params[:ratings] || {} 
+    ratings_list = @ratings_to_show
+    session[:ratings]= @ratings_to_show #part3
+    if @ratings_to_show == {}
+      ratings_list = Hash[@all_ratings.map {|x| [x, 1]}] #assign any value
     end
     
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    #update movies filtered by ratings
+    
+    @movies = Movie.with_ratings(ratings_list.keys)
+    ######
+    @clicked_header = params[:clicked_header] || "" #session[:clicked_header] || ""
+    session[:clicked_header] = @clicked_header #part3
+    #sort movies in order
+    if @clicked_header == "title_header"
+      @movies = @movies.order(:title)
     end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+    if @clicked_header == "release_date_header"
+      @movies = @movies.order(:release_date)
+    end
   end
 
   def new
@@ -34,7 +38,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -45,7 +49,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -56,18 +60,11 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
-  def same_director
-    @movie = Movie.find(params[:movie_id])
-    @director = @movie.director
-    
-    if @director.blank?
-      flash[:notice] = "'#{@movie.title}' has no director info"
-      redirect_to movies_path
-    else
-      @movies = Movie.same_director(@director)
-    end
-    
-  end
 
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 end
